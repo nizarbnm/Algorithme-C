7690247a2468ccf47cd2b7f43153d85adb8416b4
Nizar Ben Mbarek <92221030+nizarbnm@users.noreply.github.com>
2022-06-24 23:56:45 +0200

    Create Struct Person.C

---
+++ Struct Person.C
@@ -0,0 +1,24 @@
+
+#include<stdlib.h>
+#include<stdio.h>
+
+struct name
+{
+char * realname;
+char * Familyname;
+float height;
+float weight;
+};
+
+int main(int argc, const char * argv[])
+{
+struct Person.realname = "Nizar";
+struct Person.Familyname = " Ben Mbarek";
+struct person.float height = 6.25;
+struct Person.float weight = 235;
+
+printf ("%s is the hero name %. He is %2f ft tall and weight %1f lbs\n", person.realname,
+          person.familyname, person.height,person.weight);
+		  return 0;
+		  
+}
